aa3bab28dd2f11432efb1f25c20e5daf9fa19235
Chris Pineda <pineda.christopher@gmail.com>
2017-03-08 09:24:34 -0500

    Additional styling, images, animations

--- src/main.js
+++ src/main.js
@@ -2,8 +2,4 @@ import TernaryTown from './game';
 
 document.addEventListener('DOMContentLoaded', () => {
   new TernaryTown();
-  window.setTimeout( () => {
-    const app = document.getElementById('app-slide');
-    app.className = 'main-app expandUp';
-  }, 600);
 });
